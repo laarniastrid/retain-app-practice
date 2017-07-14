8a9b103952984cb7e12923ae21760c89ed87f879
Astrid Sutton <astrid.sutton@wgu.edu>
2017-07-14 10:46:33 -0600

    module-3

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
- NgModule }                from '@angular/core'
+import { NgModule }                from '@angular/core'
 import { BrowserModule }           from '@angular/platform-browser'
 import { platformBrowserDynamic }  from '@angular/platform-browser-dynamic'
 import { App }                     from './app'
 import { Main }                    from './app/containers';
+import { AppBar }                  from './app/ui';
 
 @NgModule({
   declarations: [
     App,
-    Main
+    Main,
+    AppBar
   ],
   imports: [BrowserModule],
   bootstrap: [App]

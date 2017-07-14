d3f7f02c60683b7122eeeb76eb2a1fc1bfd2f76f
Astrid Sutton <astrid.sutton@wgu.edu>
2017-07-14 10:15:42 -0600

    container components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
+import { Component }     from '@angular/core';
+
 @Component ({
     selector: 'app',
     template: `
         <div>
-            <h3>
-                yo, world!
-            </h3>
+            <main-container></main-container>
         </div>
     `
 })
